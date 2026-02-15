f9e72d6327bdf6073c8ed9d03377b177875020d8
HK Sarma <kashyapharshit50@gmail.com>
2026-02-15 22:11:27 +0530

    add: init_db script is made and ran to initialise the db with relations

---
+++ app/init_db.py
@@ -0,0 +1,12 @@
+import asyncio
+from app.db import engine
+from app.models import Base, Task
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        
+
+if __name__=="__main__":
+    asyncio.run(init_db())
