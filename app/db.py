97bfefe85fbe7e05d8983929217fd5ae8e8a66a0
HK Sarma <kashyapharshit50@gmail.com>
2026-02-15 16:23:48 +0530

    chore: db engine and sessionmaker is setup async

--- app/db.py
+++ app/db.py
@@ -1,1 +1,26 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+from contextlib import contextmanager
+from config import DB_HOST, DB_DATABASE, DB_PASSWORD, DB_PORT, DB_USER
+
+ASYNC_DATABASE_URL = (
+    f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
+)
+
+engine = create_async_engine(
+    ASYNC_DATABASE_URL,
+    pool_size=20,
+    max_overflow=10,
+    echo=True
+)
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+
+)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
