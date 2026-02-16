2fcc59e028c4909445d6674e7ac9a3fb5b73e39e
HK Sarma <kashyapharshit50@gmail.com>
2026-02-16 13:39:53 +0530

    update: docker-compose.yml

--- app/models.py
+++ app/models.py
@@ -30,24 +30,3 @@ class Task(Base):
     retry_count: Mapped[int] = mapped_column(default=0)
 
 
-# class User(Base):
-#     __tablename__ = "user_account"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     name: Mapped[str] = mapped_column(String(30))
-#     fullname: Mapped[Optional[str]] = mapped_column(String(50))
-#     addresses: Mapped[List["Addresses"]] = relationship(
-#         back_populates="user", cascade="all, delete-orphan"
-#     )
-#     def __repr__(self)->str:
-#         return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
-
-
-# class Addresses(Base):
-#     __tablename__="address"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     email: Mapped[str] = mapped_column(String(30))
-#     user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
-#     user: Mapped["User"] = relationship(back_populates="addresses")
-
-#     def __repr__(self)->str:
-#         return f"Addresses(id={self.id!r}), email_address={self.email!r}"
